2406c809a7e36cb738fc2f578eddfa58766e754f
Marcialsohfos <marcialteda@gmail.com>
2026-03-01 13:56:50 +0100

    Delete netlify/functions/supabase-config.js

--- netlify/functions/supabase-config.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/supabase-config.js
-// CE FICHIER NE SERA PAS COMMITÉ SUR GITHUB
-// Ajoutez-le à .gitignore
-
-module.exports = {
-    supabaseUrl: 'https://fbwfollnqdavorcvizxq.supabase.co',
-    supabaseServiceKey: 'sb_secret_Sm7BUoW9CKDGRzX_5LHxNQ_RmeejInq'
-};
