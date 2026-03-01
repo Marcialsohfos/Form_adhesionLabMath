ef2a2344cc29c60b2e7c07ab9a655507dbfa775b
Marcialsohfos <marcialteda@gmail.com>
2026-03-01 13:43:02 +0100

    Delete netlify/functions/supabase-config.example.js

--- netlify/functions/supabase-config.example.js
+++
@@ -1,6 +0,0 @@
-// netlify/functions/supabase-config.example.js
-// Copiez ce fichier vers supabase-config.js et mettez vos vraies clés
-module.exports = {
-    supabaseUrl: 'https://votre-projet.supabase.co',
-    supabaseServiceKey: 'votre-clé-service-role-ici'
-};
